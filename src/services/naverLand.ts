ffc522d1db64d371ed02c69e5312d0ecd9269f6d
Hoon Namkoong <hoonnamkoong@example.com>
2025-12-22 15:56:20 +0900

    Enhance: Expand Precise Search Coverage to All 25 Seoul Districts

--- src/services/naverLand.ts
+++ src/services/naverLand.ts
@@ -15,46 +15,213 @@ export class NaverLandService {
 
     // Dong Coordinates Registry (Approximate Centers)
     private DONG_REGISTRY: Record<string, { name: string; lat: number; lon: number }[]> = {
+        // --- GANGNAM 3-GU ---
         '1171000000': [ // Songpa-gu
-            { name: '잠실', lat: 37.512, lon: 127.094 },
-            { name: '신천', lat: 37.520, lon: 127.105 },
-            // Pungnap - Split into North/South to cover elongated shape
-            { name: '풍납1', lat: 37.538, lon: 127.123 }, // Near City Geukdong/Pungnap Toseong
-            { name: '풍납2', lat: 37.529, lon: 127.117 }, // Near Pungnap Baekje/Riverville
-            { name: '풍납3', lat: 37.522, lon: 127.110 }, // Near Asan Hospital/Pungnap 2-dong
-            { name: '송파', lat: 37.505, lon: 127.115 },
-            { name: '석촌', lat: 37.503, lon: 127.105 },
+            { name: '잠실1(엘스/리센츠)', lat: 37.512, lon: 127.085 },
+            { name: '잠실2(트리지움/레이크)', lat: 37.509, lon: 127.093 },
+            { name: '잠실3(아시아선수촌)', lat: 37.508, lon: 127.075 },
+            { name: '잠실4(파크리오)', lat: 37.521, lon: 127.103 },
+            { name: '신천(장미)', lat: 37.518, lon: 127.103 },
+            { name: '풍납1(북부)', lat: 37.538, lon: 127.123 },
+            { name: '풍납2(중부)', lat: 37.529, lon: 127.117 },
+            { name: '풍납3(남부)', lat: 37.522, lon: 127.110 },
+            { name: '방이1(먹자골목)', lat: 37.513, lon: 127.110 },
+            { name: '방이2(올림픽공원)', lat: 37.512, lon: 127.125 },
+            { name: '송파1(송파동)', lat: 37.505, lon: 127.115 },
+            { name: '석촌(석촌호수)', lat: 37.503, lon: 127.105 },
             { name: '삼전', lat: 37.502, lon: 127.090 },
-            { name: '방이', lat: 37.515, lon: 127.125 },
-            { name: '오금', lat: 37.505, lon: 127.135 },
-            { name: '가락', lat: 37.495, lon: 127.125 },
-            { name: '문정', lat: 37.485, lon: 127.125 },
-            { name: '장지', lat: 37.478, lon: 127.135 },
-            { name: '거여', lat: 37.495, lon: 127.145 },
+            { name: '오금', lat: 37.503, lon: 127.135 },
+            { name: '가락1(헬리오시티)', lat: 37.496, lon: 127.108 },
+            { name: '가락2(가락시장)', lat: 37.493, lon: 127.125 },
+            { name: '문정1(로데오)', lat: 37.488, lon: 127.120 },
+            { name: '문정2(법조타운)', lat: 37.483, lon: 127.120 },
+            { name: '장지', lat: 37.478, lon: 127.130 },
+            { name: '거여', lat: 37.493, lon: 127.145 },
             { name: '마천', lat: 37.495, lon: 127.155 }
         ],
         '1168000000': [ // Gangnam-gu
-            { name: '압구정', lat: 37.528, lon: 127.028 },
-            { name: '신사', lat: 37.522, lon: 127.022 },
-            { name: '청담', lat: 37.522, lon: 127.045 },
+            { name: '압구정1(구현대)', lat: 37.530, lon: 127.028 },
+            { name: '압구정2(신현대/미성)', lat: 37.528, lon: 127.020 },
+            { name: '신사', lat: 37.520, lon: 127.022 },
             { name: '논현', lat: 37.512, lon: 127.030 },
-            { name: '삼성', lat: 37.512, lon: 127.058 },
-            { name: '역삼', lat: 37.500, lon: 127.038 },
-            { name: '대치', lat: 37.498, lon: 127.060 },
-            { name: '도곡', lat: 37.490, lon: 127.045 },
-            { name: '개포', lat: 37.480, lon: 127.065 },
+            { name: '청담', lat: 37.522, lon: 127.045 },
+            { name: '삼성1(코엑스)', lat: 37.512, lon: 127.058 },
+            { name: '삼성2(선정릉)', lat: 37.510, lon: 127.045 },
+            { name: '역삼1(역세권)', lat: 37.500, lon: 127.035 },
+            { name: '역삼2(주거)', lat: 37.495, lon: 127.045 },
+            { name: '대치1(학원가)', lat: 37.495, lon: 127.060 },
+            { name: '대치2(은마)', lat: 37.498, lon: 127.068 },
+            { name: '도곡1(타워팰리스)', lat: 37.488, lon: 127.052 },
+            { name: '도곡2(매봉)', lat: 37.485, lon: 127.040 },
+            { name: '개포', lat: 37.480, lon: 127.060 },
             { name: '일원', lat: 37.485, lon: 127.085 },
             { name: '수서', lat: 37.488, lon: 127.102 },
             { name: '세곡', lat: 37.465, lon: 127.105 }
         ],
         '1165000000': [ // Seocho-gu
             { name: '잠원', lat: 37.515, lon: 127.015 },
-            { name: '반포', lat: 37.505, lon: 127.000 },
-            { name: '서초', lat: 37.488, lon: 127.015 },
-            { name: '방배', lat: 37.482, lon: 126.995 },
-            { name: '양재', lat: 37.482, lon: 127.038 },
-            { name: '우면', lat: 37.465, lon: 127.025 },
-            { name: '내곡', lat: 37.455, lon: 127.065 }
+            { name: '반포1(한강변)', lat: 37.508, lon: 127.000 },
+            { name: '반포2(터미널)', lat: 37.505, lon: 127.012 },
+            { name: '반포3(서래)', lat: 37.498, lon: 126.995 },
+            { name: '서초1(법원)', lat: 37.493, lon: 127.010 },
+            { name: '서초2(남부)', lat: 37.483, lon: 127.015 },
+            { name: '방배1(내방)', lat: 37.488, lon: 126.990 },
+            { name: '방배2(사당)', lat: 37.478, lon: 126.985 },
+            { name: '양재', lat: 37.478, lon: 127.040 },
+            { name: '우면', lat: 37.465, lon: 127.025 }
+        ],
+        '1174000000': [ // Gangdong-gu
+            { name: '고덕1(그라시움)', lat: 37.555, lon: 127.170 },
+            { name: '고덕2(비즈밸리)', lat: 37.565, lon: 127.170 },
+            { name: '상일', lat: 37.550, lon: 127.175 },
+            { name: '명일', lat: 37.550, lon: 127.150 },
+            { name: '암사', lat: 37.550, lon: 127.130 },
+            { name: '천호', lat: 37.540, lon: 127.125 },
+            { name: '성내', lat: 37.530, lon: 127.125 },
+            { name: '길동', lat: 37.535, lon: 127.140 },
+            { name: '둔촌(올림픽파크)', lat: 37.525, lon: 127.140 }
+        ],
+
+        // --- MA-YONG-SEONG ---
+        '1144000000': [ // Mapo-gu
+            { name: '공덕/아현', lat: 37.548, lon: 126.953 },
+            { name: '도화/마포', lat: 37.540, lon: 126.945 },
+            { name: '용강/대흥', lat: 37.545, lon: 126.940 },
+            { name: '상암', lat: 37.575, lon: 126.890 },
+            { name: '성산', lat: 37.565, lon: 126.910 },
+            { name: '합정/망원', lat: 37.550, lon: 126.910 },
+            { name: '연남/동교', lat: 37.560, lon: 126.925 }
+        ],
+        '1117000000': [ // Yongsan-gu
+            { name: '이촌1(동부)', lat: 37.520, lon: 126.980 },
+            { name: '이촌2(서부)', lat: 37.525, lon: 126.960 },
+            { name: '한남/이태원', lat: 37.535, lon: 127.000 },
+            { name: '서빙고', lat: 37.520, lon: 126.995 },
+            { name: '용산역/한강로', lat: 37.530, lon: 126.965 },
+            { name: '후암/남영', lat: 37.545, lon: 126.975 },
+            { name: '효창/원효', lat: 37.540, lon: 126.960 }
+        ],
+        '1120000000': [ // Seongdong-gu
+            { name: '성수1(서울숲)', lat: 37.545, lon: 127.040 },
+            { name: '성수2(전략정비)', lat: 37.538, lon: 127.055 },
+            { name: '옥수', lat: 37.541, lon: 127.017 },
+            { name: '금호', lat: 37.548, lon: 127.023 },
+            { name: '왕십리/행당', lat: 37.561, lon: 127.037 },
+            { name: '마장', lat: 37.566, lon: 127.042 }
+        ],
+
+        // --- SEOUL CENTRAL (GANGBUK) ---
+        '1111000000': [ // Jongno-gu
+            { name: '평창/구기', lat: 37.605, lon: 126.965 },
+            { name: '광화문/사직', lat: 37.575, lon: 126.970 },
+            { name: '혜화/이화', lat: 37.580, lon: 127.000 },
+            { name: '창신/숭인', lat: 37.575, lon: 127.015 }
+        ],
+        '1114000000': [ // Jung-gu
+            { name: '신당/황학', lat: 37.565, lon: 127.018 },
+            { name: '약수/청구', lat: 37.555, lon: 127.012 },
+            { name: '중림/회현', lat: 37.558, lon: 126.968 }
+        ],
+        '1123000000': [ // Dongdaemun-gu
+            { name: '청량리', lat: 37.582, lon: 127.048 },
+            { name: '전농/답십리', lat: 37.575, lon: 127.055 },
+            { name: '장안', lat: 37.570, lon: 127.070 },
+            { name: '이문/휘경', lat: 37.595, lon: 127.065 }
+        ],
+        '1121500000': [ // Gwangjin-gu
+            { name: '광장', lat: 37.542, lon: 127.103 },
+            { name: '구의', lat: 37.540, lon: 127.085 },
+            { name: '자양', lat: 37.535, lon: 127.070 },
+            { name: '화양/군자', lat: 37.550, lon: 127.075 },
+            { name: '중곡', lat: 37.565, lon: 127.085 }
+        ],
+
+        // --- SOUTHWEST (YEO-YANG-DONG) ---
+        '1156000000': [ // Yeongdeungpo-gu
+            { name: '여의도', lat: 37.525, lon: 126.930 },
+            { name: '당산', lat: 37.535, lon: 126.900 },
+            { name: '영등포/문래', lat: 37.518, lon: 126.900 },
+            { name: '신길', lat: 37.505, lon: 126.915 }
+        ],
+        '1147000000': [ // Yangcheon-gu
+            { name: '목동1(앞단지)', lat: 37.535, lon: 126.885 },
+            { name: '목동2(뒷단지)', lat: 37.520, lon: 126.870 },
+            { name: '신정', lat: 37.515, lon: 126.855 },
+            { name: '신월', lat: 37.525, lon: 126.835 }
+        ],
+        '1159000000': [ // Dongjak-gu
+            { name: '흑석', lat: 37.508, lon: 126.963 },
+            { name: '노량진', lat: 37.512, lon: 126.942 },
+            { name: '상도', lat: 37.498, lon: 126.945 },
+            { name: '사당', lat: 37.485, lon: 126.972 },
+            { name: '대방/신대방', lat: 37.500, lon: 126.925 }
+        ],
+        '1162000000': [ // Gwanak-gu
+            { name: '봉천/서울대', lat: 37.482, lon: 126.952 },
+            { name: '신림1(역세권)', lat: 37.485, lon: 126.930 },
+            { name: '신림2(난곡)', lat: 37.470, lon: 126.918 }
+        ],
+        '1150000000': [ // Gangseo-gu
+            { name: '마곡1(지구)', lat: 37.565, lon: 126.830 },
+            { name: '가양/등촌', lat: 37.558, lon: 126.855 },
+            { name: '염창', lat: 37.550, lon: 126.870 },
+            { name: '화곡', lat: 37.540, lon: 126.845 },
+            { name: '방화', lat: 37.575, lon: 126.815 }
+        ],
+        '1153000000': [ // Guro-gu
+            { name: '신도림', lat: 37.508, lon: 126.880 },
+            { name: '구로', lat: 37.495, lon: 126.885 },
+            { name: '개봉/고척', lat: 37.495, lon: 126.855 }
+        ],
+        '1154500000': [ // Geumcheon-gu
+            { name: '가산/독산', lat: 37.470, lon: 126.895 },
+            { name: '시흥', lat: 37.450, lon: 126.905 }
+        ],
+
+        // --- NORTHEAST (NO-DO-GANG) ---
+        '1135000000': [ // Nowon-gu
+            { name: '상계', lat: 37.660, lon: 127.065 },
+            { name: '중계', lat: 37.645, lon: 127.075 },
+            { name: '하계', lat: 37.635, lon: 127.070 },
+            { name: '공릉', lat: 37.625, lon: 127.075 },
+            { name: '월계', lat: 37.625, lon: 127.055 }
+        ],
+        '1132000000': [ // Dobong-gu
+            { name: '창동', lat: 37.650, lon: 127.045 },
+            { name: '방학', lat: 37.665, lon: 127.035 },
+            { name: '쌍문', lat: 37.650, lon: 127.035 },
+            { name: '도봉', lat: 37.680, lon: 127.045 }
+        ],
+        '1130500000': [ // Gangbuk-gu
+            { name: '미아1(뉴타운)', lat: 37.620, lon: 127.020 },
+            { name: '미아2(사거리)', lat: 37.613, lon: 127.030 },
+            { name: '수유/번동', lat: 37.640, lon: 127.025 }
+        ],
+        '1129000000': [ // Seongbuk-gu
+            { name: '길음/뉴타운', lat: 37.605, lon: 127.020 },
+            { name: '성북/돈암', lat: 37.595, lon: 127.015 },
+            { name: '종암/월곡', lat: 37.600, lon: 127.035 },
+            { name: '석관/장위', lat: 37.615, lon: 127.055 }
+        ],
+        '1126000000': [ // Jungnang-gu
+            { name: '상봉/망우', lat: 37.595, lon: 127.090 },
+            { name: '면목', lat: 37.580, lon: 127.085 },
+            { name: '신내', lat: 37.615, lon: 127.095 },
+            { name: '중화/묵동', lat: 37.605, lon: 127.075 }
+        ],
+
+        // --- NORTHWEST ---
+        '1138000000': [ // Eunpyeong-gu
+            { name: '은평1(뉴타운)', lat: 37.640, lon: 126.920 },
+            { name: '녹번/응암', lat: 37.600, lon: 126.925 },
+            { name: '연신내/불광', lat: 37.620, lon: 126.920 },
+            { name: '수색/증산', lat: 37.580, lon: 126.895 }
+        ],
+        '1141000000': [ // Seodaemun-gu
+            { name: '가재울/DMC', lat: 37.575, lon: 126.915 },
+            { name: '홍제/무악', lat: 37.585, lon: 126.945 },
+            { name: '신촌/연희', lat: 37.560, lon: 126.935 },
+            { name: '북아현', lat: 37.560, lon: 126.955 }
         ]
     };
 
@@ -63,7 +230,7 @@ export class NaverLandService {
         // Songpa-gu (11710)
         '1171010100': '잠실',
         '1171010200': '신천',
-        '1171010300': '풍납', // Fixed: Hangang Geukdong is here
+        '1171010300': '풍납',
         '1171010400': '송파',
         '1171010500': '석촌',
         '1171010600': '삼전',
@@ -73,7 +240,7 @@ export class NaverLandService {
         '1171011100': '방이',
         '1171011200': '오금',
         '1171011300': '거여',
-        '1171011400': '마천', // Fixed: Macheon Kumho is here
+        '1171011400': '마천',
 
         // Gangnam-gu (11680)
         '1168010100': '역삼',
@@ -282,11 +449,6 @@ export class NaverLandService {
             return [];
         }
     }
-
-    /**
-     * Get Article Detail (To get households if needed, but expensive)
-     * Skipped for now.
-     */
 
     /**
      * Get Region Code (CortarNo)
